b615b8467e0af57f84c679d4e5460f22653a6a94
sujit1984 <sujitsugathan2002@gmail.com>
2022-10-31 20:52:22 +0530

    Updated the comments

--- DSA_In_Python/Algorithms/Recursion/factorial.py
+++ DSA_In_Python/Algorithms/Recursion/factorial.py
@@ -1,9 +1,15 @@
+"""
+Author: Sujit Sugathan
+Date: 31 Oct 2022
+Description: This function is a simple recursive function to calculate the factorial of n.
+"""
+
 def factorial(n):
-    assert n >=0 and int(n) == n, "The number must be a positive integer"
+    assert n >=0 and int(n) == n, "The number must be a positive integer" ## check for non-negative integers
     if n in (0,1):
         return 1
     else: 
-        return n * factorial(n-1)
+        return n * factorial(n-1) # recursive function
 
 if __name__ == "__main__":
     num = int(input("Enter the number for factorial:"))
